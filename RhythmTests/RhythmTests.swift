703da4f4a3abd636d180c7f6cab339faae0bafe5
DengNaiChen <dengnaichen@gmail.com>
2026-03-12 21:28:47 -0400

    Initial Commit

---
+++ RhythmTests/RhythmTests.swift
@@ -0,0 +1,17 @@
+//
+//  RhythmTests.swift
+//  RhythmTests
+//
+//  Created by Naicheng Deng on 2026-03-12.
+//
+
+import Testing
+@testable import Rhythm
+
+struct RhythmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
